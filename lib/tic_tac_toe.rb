21cf656cc24a1ff5d8769713bfa6839e46c7955c
Muhammad mahdi <danyalm50@gmail.com>
2020-11-08 20:18:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,7 +29,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
+  index = index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
